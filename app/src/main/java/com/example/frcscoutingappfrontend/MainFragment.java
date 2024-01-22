545235c7ccaaa6fd81462bb9adc306016009e675
Goomisica <60203441+Goomisica@users.noreply.github.com>
2024-01-21 16:25:54 -0800

    made things not clickable when popup
    literally one line of code :skull:

--- app/src/main/java/com/example/frcscoutingappfrontend/MainFragment.java
+++ app/src/main/java/com/example/frcscoutingappfrontend/MainFragment.java
@@ -12,6 +12,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.service.autofill.OnClickAction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
