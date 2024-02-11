90dd0eede4acd6ac56a871bcbca362eccefe1b43
JAKlein26 <JAKlein26@chaminet.org>
2024-02-10 18:08:40 -0800

    Did alot of Bluetooth and it still doesn't work. :(

--- app/src/main/java/com/example/frcscoutingappfrontend/MainActivity.java
+++ app/src/main/java/com/example/frcscoutingappfrontend/MainActivity.java
@@ -7,16 +7,20 @@ import androidx.fragment.app.FragmentTransaction;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothSocket;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.net.MacAddress;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
+import java.io.IOException;
 import java.util.Set;
+import java.util.UUID;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -24,9 +28,12 @@ public class MainActivity extends AppCompatActivity {
     TeleopFragment secondaryFragment = new TeleopFragment();
     ConfirmPopout popoutFragment = new ConfirmPopout();
     BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
-
+    ConnectThread connectThread;
     //Broadcast Receiver for Bluetooth
     private static final int REQUEST_ENABLE_BLUETOOTH = 2;
+    private static final String ExternalMACAdress = "14:4F:8A:90:90:9C";
+    private static final UUID MY_UUID = UUID.fromString("756822d2-28b3-4bba-af41-49f76f14b186");
+    private static final UUID MY2_UUID = new UUID(0,0);
     Set<BluetoothDevice> ad;
     StringBuilder sb = new StringBuilder();
 
@@ -50,15 +57,19 @@ public class MainActivity extends AppCompatActivity {
         //Bluetooth Code
 
         //makes sure bluetooth exists
-        if(adapter == null) {
+        if (adapter == null) {
             Toast.makeText(this, "Bluetooth no workie :(", Toast.LENGTH_LONG).show();
-        }
-        else {
+        } else {
             Toast.makeText(this, "Bluetooth workie!!", Toast.LENGTH_LONG).show();
         }
     }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        connectThread.cancel();
+    }
     public void enableConnectBT() {
-        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
             Toast.makeText(this, "Bluetooth not allowed :(", Toast.LENGTH_LONG).show();
             return;
         }
@@ -68,14 +79,83 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "Bluetooth enabled!", Toast.LENGTH_LONG).show();
         } else {
             ad = adapter.getBondedDevices();
-            if(ad == null) {
+            if (ad == null) {
                 Toast.makeText(this, "No Devices to Connect To", Toast.LENGTH_LONG).show();
                 return;
             }
             for (BluetoothDevice temp : ad) {
-                Toast.makeText(this, temp.getName(), Toast.LENGTH_SHORT).show();
+                if(temp.getAddress().equals(ExternalMACAdress)) {
+                    connectThread = new ConnectThread(temp, this);
+                    Toast.makeText(this, "connected to: "+temp.getName(), Toast.LENGTH_SHORT).show();
+                    connectThread.run();
+                    return;
+                }
             }
         }
     }
 
+    // for connecting to central laptop
+    private class ConnectThread extends Thread {
+        private final BluetoothSocket mmSocket;
+        private final BluetoothDevice mmDevice;
+        private Context context;
+        public ConnectThread(BluetoothDevice device, Context context) {
+            // Use a temporary object that is later assigned to mmSocket
+            // because mmSocket is final.
+            BluetoothSocket tmp = null;
+            mmDevice = device;
+            this.context = context;
+            try {
+                // Get a BluetoothSocket to connect with the given BluetoothDevice.
+                // MY_UUID is the app's UUID string, also used in the server code.
+                if (ActivityCompat.checkSelfPermission(getBaseContext(), android.Manifest.permission.BLUETOOTH_CONNECT) == PackageManager.PERMISSION_GRANTED) {
+                    tmp = device.createInsecureRfcommSocketToServiceRecord(MY_UUID);
+                            //createRfcommSocketToServiceRecord(MY_UUID);
+                }
+            } catch (IOException e) {
+                Toast.makeText(getBaseContext(), "couldn't create server", Toast.LENGTH_LONG).show();
+            }
+            mmSocket = tmp;
+        }
+
+        @Override
+        public void run() {
+            // Cancel discovery because it otherwise slows down the connection.
+            if (ActivityCompat.checkSelfPermission(getBaseContext(), android.Manifest.permission.BLUETOOTH_SCAN) == PackageManager.PERMISSION_GRANTED) {
+                adapter.cancelDiscovery();
+            }
+
+            try {
+                // Connect to the remote device through the socket. This call blocks
+                // until it succeeds or throws an exception.
+                if (ActivityCompat.checkSelfPermission(getBaseContext(), android.Manifest.permission.BLUETOOTH_CONNECT) == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(context, "HELLOWORLD", Toast.LENGTH_LONG).show();
+                    mmSocket.connect();
+                    Toast.makeText(context, "ROBERTBADLETTTT", Toast.LENGTH_LONG).show();
+                }
+            } catch (IOException connectException) {
+                Toast.makeText(context, "Flip dude: "+connectException, Toast.LENGTH_LONG).show();
+                // Unable to connect; close the socket and return.
+                try {
+                    mmSocket.close();
+                } catch (IOException closeException) {
+                    Toast.makeText(getBaseContext(), closeException.toString(), Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+
+            // The connection attempt succeeded. Perform work associated with
+            // the connection in a separate thread.
+//            manageMyConnectedSocket(mmSocket);
+        }
+
+        // Closes the client socket and causes the thread to finish.
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) {
+                Toast.makeText(getBaseContext(),"couldn't close client BT socket, "+e.toString(),Toast.LENGTH_LONG).show();
+            }
+        }
+    }
 }
