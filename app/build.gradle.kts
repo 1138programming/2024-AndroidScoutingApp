ad1e0a3632cfb3576f1d8c9e54188d13314ef086
Goomisica <60203441+Goomisica@users.noreply.github.com>
2024-03-14 20:59:41 -0700

    need to lower apk version for newer tablets (they won't update)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.frcscoutingappfrontend"
-        minSdk = 33
+        minSdk = 31
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
